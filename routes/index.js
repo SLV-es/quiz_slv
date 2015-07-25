2d6ebdde52b10de5b2661807a199f7ae88b9f0bc
SLV-es <salva@open-office.es>
2015-07-25 17:57:44 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ var quizController = require('../controllers/quiz_controller');
 var quizCreditos = require('../creditos/quiz_creditos');
 
 /* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz-SLV' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                quizController.index);
+router.get('/quizes/:quizId',        quizController.show);
+router.get('/quizes/:quizId/answer', quizController.answer);
+
+// Definición de rutas de /creditos
 router.get('/creditos/credito', quizCreditos.credito);
 
 module.exports = router;
